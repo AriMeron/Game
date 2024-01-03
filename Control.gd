db9979a18d0b341a2febfb6dd89e684c187d7a6d
David <dchen1561@gmail.com>
2024-01-03 12:35:26 -0800

    Initial Main Menu outline
    Put your code in here

---
+++ Control.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_view_assets_button_down():
+	pass # Replace with function body.
+
+
+func _on_load_map_button_down():
+	pass # Replace with function body.
+
+
+func _on_host_button_down():
+	pass # Replace with function body.
+
+
+func _on_client_button_down():
+	pass # Replace with function body.
+
+
+func _on_test_movement_button_down():
+	pass # Replace with function body.
+
+
+func _on_load_menus_button_down():
+	pass # Replace with function body.
