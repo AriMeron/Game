ea9be15b477a39f736ab5d8b5f6e9853b1e95d33
skiergr <80352508+skiergr@users.noreply.github.com>
2024-01-09 14:32:25 -0800

    Host and Join Working
    Connected Players from two instances working. Hosting on local IP.

--- Control.gd
+++ Control.gd
@@ -1,10 +1,54 @@
 extends Control
 
+@export var Address = "127.0.0.1"
+@export var port = 8910
+var peer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
+	
 	pass # Replace with function body.
 
+func peer_connected(id):
+	print("Player Connected " + str(id))
+
+func peer_disconnected(id):
+	print("Player Disconnected " + str(id))
+
+func connected_to_server():
+	print("Connected to Server!")
+
+func connection_failed(id):
+	print("Couldn't Connect")
+
+func _on_host_button_down():
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port, 6)
+	if error != OK:
+		print("cannont host:" + error)
+		return
+	
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("Waiting For Players!")
+	
+	pass # Replace with function body.
+
+func _on_start_game_button_down():
+	pass # Replace with function body.
+
+
+func _on_join_button_down():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(Address, port)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,22 +62,8 @@ func _on_view_assets_button_down():
 func _on_load_map_button_down():
 	pass # Replace with function body.
 
-
-func _on_host_button_down():
-	pass # Replace with function body.
-
-
-func _on_client_button_down():
-	pass # Replace with function body.
-
-
 func _on_test_movement_button_down():
 	pass # Replace with function body.
-
-
-func _on_load_menus_button_down():
-	pass # Replace with function body.
-
 
 func _on_node_load_example_button_down():
 	var scene = load("res://Scenes/Menus/ExampleScene.tscn").instantiate()
