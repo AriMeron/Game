bdfcd2916656d93f2d8985f1af7b3d4f4904f7d2
skiergr <80352508+skiergr@users.noreply.github.com>
2024-01-09 18:29:48 -0800

    Multiplayer Synchronizer
    Only position

--- Scenes/Player/SceneManager.gd
+++ Scenes/Player/SceneManager.gd
@@ -7,6 +7,7 @@ func _ready():
 	var index = 0
 	for i in GameManager.Players:
 		var currentPlayer = PlayerScene.instantiate()
+		currentPlayer.name = str(GameManager.Players[i].id)
 		add_child(currentPlayer)
 		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoint"):
 			if spawn.name == str(index):
