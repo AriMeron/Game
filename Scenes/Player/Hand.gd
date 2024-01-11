150d8c46d6f8256dd0027cfc76576eac5fa1423c
Natalie Lim <natalielim1906@gmail.com>
2024-01-11 14:35:25 -0800

    gun in 4 directions

--- Scenes/Player/Hand.gd
+++ Scenes/Player/Hand.gd
@@ -1,7 +1,16 @@
 extends CharacterBody3D
 
+const ROTATION_SPEED = 10
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		transform.origin = Vector3 (0.06, -0.02, 0);
 	if Input.is_action_pressed("ui_left"):
 		transform.origin = Vector3 (-0.06, -0.02, 0);
+	if Input.is_action_pressed("ui_down"):
+		transform.origin = Vector3 (-0.06, -0.02, 0);
+	if Input.is_action_pressed("ui_up"):
+		transform.origin = Vector3 (-0.06, -0.02, 0);
+	
+	var rotation_amount = Input.get_action_strength("turn") * ROTATION_SPEED * delta
+	rotate_z(rotation_amount)
--- Scenes/Player/gun.gd
+++ Scenes/Player/gun.gd
@@ -22,9 +22,9 @@ func _process(delta):
 		if Input.is_action_pressed("ui_left"):
 			transform.origin = Vector3 (-0.14, -0.06, 0);
 		if Input.is_action_pressed("ui_down"):
-			transform.origin = Vector3 (-0.223, -0.06, 0);
+			transform.origin = Vector3 (-0.14, -0.06, 0);
 		if Input.is_action_pressed("ui_up"):
-			transform.origin = Vector3 (-0.223, -0.06, 0);
+			transform.origin = Vector3 (-0.14, -0.06, 0);
 
 	# Apply movement using velocity
 	if velocity.length() > 0:
