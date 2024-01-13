9e50e84503cd93ef48656ce492b4e8590a66327b
Natalie Lim <natalielim1906@gmail.com>
2024-01-12 22:27:10 -0800

    cleaner code

--- Scenes/Player/gun.gd
+++ Scenes/Player/gun.gd
@@ -1,17 +1,27 @@
 extends CharacterBody3D
-#
+
 var sensitivity = 0.1
+var gun_offset = Vector3()  # Initialize gun_offset as a Vector3
+const ROTATION_SPEED = 10
+var side_name = "right"
+var rolling = false
+var roll_timer = 0.5
+
+func set_gun_offset(offset: Vector3):
+	gun_offset = offset
 
 func _process(delta):
+
 	var screen_size = get_viewport().get_visible_rect().size
 	var mouse_position = get_viewport().get_mouse_position()
 
 	# Calculate the direction vector from character to mouse
-	var character_position = global_transform.origin
+	# Ensure that gun_offset is a Vector3
+	var character_position = global_transform.origin + gun_offset
 	var target_direction = (mouse_position - screen_size / 2).normalized()
 
 	# Calculate the angle between the current direction and the target direction
 	var angle = atan2(target_direction.y, target_direction.x)
 
 	# Set the character's rotation to point towards the mouse
-	set_rotation(Vector3(0, 0, -angle))
+	set_rotation(Vector3(0, 0, -angle - 3.1415))
