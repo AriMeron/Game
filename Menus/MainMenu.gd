2917de1d7a0086e6c19796af7c7b4b82c83276f6
Andrew Theiss <andrew.theiss@gmail.com>
2024-01-12 20:50:01 -0800

    Create main menu wtih proper orientation
    Followed Godot 4 Online Multiplayer FPS from scratch

---
+++ Menus/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_host_button_down():
+	# initialize as host
+	pass # Replace with function body.
+
+func _on_start_game_button_down():
+	# Start game StartGame.rpc()
+	pass # Replace with function body.
+
+
+func _on_join_button_down():
+	# initialize as client
+	pass
