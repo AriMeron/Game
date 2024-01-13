5063b7a8a79f43728465e7ddb95f925b0cdb2e3e
Natalie Lim <natalielim1906@gmail.com>
2024-01-12 22:32:43 -0800

    fixed janky hand-switching

--- Scenes/Player/Hand.gd
+++ Scenes/Player/Hand.gd
@@ -6,30 +6,25 @@ var rolling = false
 var roll_timer = 0.5
 
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		# Right, Up-Right, or Up
-		if Input.is_action_pressed("ui_down"): 
-			side_name = "right"
+	if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_down"):
+			side_name = "left"  # Left and Down pressed
 		elif Input.is_action_pressed("ui_up"):
-			side_name= "right"
+			side_name = "left"  # Left and Up pressed
 		else:
-			side_name = "right"
-	elif Input.is_action_pressed("ui_left"):
-		# Left, Up-Left, or Up coming from the left side
-		if Input.is_action_pressed("ui_down"): 
-			side_name = "left"
+			side_name = "left"  # Only Left pressed
+	elif Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("ui_down"):
+			side_name = "right"  # Right and Down pressed
 		elif Input.is_action_pressed("ui_up"):
-			side_name = "left"
+			side_name = "right"  # Right and Up pressed
 		else:
-			side_name = "left"
-	elif Input.is_action_pressed("ui_down"):
-		side_name = "right"
-	elif Input.is_action_pressed("ui_up"):
-		side_name = "left"
+			side_name = "right"  # Only Right pressed
+	
 	switch()
 
 func switch():
 	if side_name == "right":
-		transform.origin = Vector3 (0.06, -0.02, 0);
+		transform.origin = Vector3(0.06, -0.02, 0)
 	else:
-		transform.origin = Vector3 (-0.06, -0.02, 0);
+		transform.origin = Vector3(-0.06, -0.02, 0)
