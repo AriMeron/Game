489db57f606a35940af84a1d4bac79e0cd5efb9d
Victor <victorli1076@gmail.com>
2024-01-14 19:50:26 -0800

    heal particles and animated particle demo

--- Scenes/Particles/Particle.gd
+++ Scenes/Particles/Particle.gd
@@ -4,9 +4,6 @@ extends GPUParticles3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	one_shot = true
-	var pm = process_material
-	pm.angle_min = -90
-	pm.angle_max = 90
 	#pass
 
 
