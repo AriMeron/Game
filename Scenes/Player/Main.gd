75ae35a16ff5f5d60326541ea9cf54ecf79fbb61
Natalie Lim <natalielim1906@gmail.com>
2024-01-16 10:42:12 -0800

    z-axis set

--- Scenes/Player/Main.gd
+++ Scenes/Player/Main.gd
@@ -41,20 +41,50 @@ func shoot_bullet():
 
 	var bullet_instance = BULLET_SCENE.instantiate()
 	add_child(bullet_instance)
+	var initial_z = 2
 	bullet_instance.global_transform.origin = camera.global_transform.origin
 
 	var mouse_pos = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(mouse_pos)
-	var to = from + camera.project_ray_normal(mouse_pos) * 1000
+	var to = from + camera.project_ray_normal(mouse_pos) * RAY_LENGTH
 	var query_parameters = PhysicsRayQueryParameters3D.new()
 	query_parameters.from = from
 	query_parameters.to = to
 	var space_state = get_world_3d().direct_space_state
 	var result = space_state.intersect_ray(query_parameters)
 
+	var target_pos
 	if result:
-		var target_pos = result.position
-		var direction = (target_pos - camera.global_transform.origin).normalized()
-		bullet_instance.linear_velocity = direction * BULLET_SPEED
+		target_pos = result.position
 	else:
-		bullet_instance.queue_free()  # Remove the bullet if it doesn't hit anything
+		# If the raycast doesn't hit anything, use the 'to' position as the target
+		target_pos = to
+	# Maintain constant Z-axis level
+	target_pos.z = initial_z
+	var direction = (target_pos - camera.global_transform.origin).normalized()
+	bullet_instance.linear_velocity = direction * BULLET_SPEED
+
+#	var camera = get_node_or_null("Camera3D")
+#	if camera == null:
+#		print("Camera node not found")
+#		return
+#
+#	var bullet_instance = BULLET_SCENE.instantiate()
+#	add_child(bullet_instance)
+#	bullet_instance.global_transform.origin = camera.global_transform.origin
+#
+#	var mouse_pos = get_viewport().get_mouse_position()
+#	var from = camera.project_ray_origin(mouse_pos)
+#	var to = from + camera.project_ray_normal(mouse_pos) * 1000
+#	var query_parameters = PhysicsRayQueryParameters3D.new()
+#	query_parameters.from = from
+#	query_parameters.to = to
+#	var space_state = get_world_3d().direct_space_state
+#	var result = space_state.intersect_ray(query_parameters)
+#
+#	if result:
+#		var target_pos = result.position
+#		var direction = (target_pos - camera.global_transform.origin).normalized()
+#		bullet_instance.linear_velocity = direction * BULLET_SPEED
+#	else:
+#		bullet_instance.queue_free()  # Remove the bullet if it doesn't hit anything
