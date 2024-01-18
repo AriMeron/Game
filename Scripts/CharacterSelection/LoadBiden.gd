81d533c41b72659d48a5afd4a069968d9f93fd5a
Ari Meron <ari@involve.ai>
2024-01-18 13:44:16 -0800

    Made Character selection screen

---
+++ Scripts/CharacterSelection/LoadBiden.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	var scene = load("res://Scenes/Menus/CharacterSelection/Biden.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
