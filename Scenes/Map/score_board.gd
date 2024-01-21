98b5da900b923112a29328ca1eef58200f3bb8e7
Natalie Lim <natalielim1906@gmail.com>
2024-01-21 12:44:43 -0800

    endgame works

--- Scenes/Map/score_board.gd
+++ Scenes/Map/score_board.gd
@@ -12,8 +12,19 @@ func _ready():
 func update_score_Dem(points: int):
 	score_Dem += points
 	score_label_Dem.text = "Democrats: " + str(score_Dem)
+	check_for_winner()
 		
 
 func update_score_Rep(points: int):
 	score_Rep += points
 	score_label_Rep.text = "Republicans: " + str(score_Rep)
+	check_for_winner()
+	
+func check_for_winner():
+	if score_Dem >=270 or score_Rep >= 270:
+		var victory_scene = load("res://Scenes/Map/victory_temp.tscn")
+		if victory_scene:
+		# Change to the new scene
+			get_tree().change_scene_to_file("res://Scenes/Map/victory_temp.tscn")
+		else:
+			print("Failed to load scene")
