45fd7fd34b45d6ba1d893b4dd5fab8434ebbabbc
skiergr <80352508+skiergr@users.noreply.github.com>
2024-01-22 12:35:57 -0800

    Main Menu Script Reverted

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -2,8 +2,6 @@ extends Control
 
 var server_script = preload("res://Scripts/Server.gd");
 var test_scene = preload("res://Scenes/Map/test_scene.tscn");
-var control_script = preload("res://Control.gd")
-var control_instance = control_script.new()
 
 func _on_settings_pressed():
 	var instance = test_scene.instantiate()
@@ -14,9 +12,10 @@ func _on_settings_pressed():
 
 
 func _on_host_lan_pressed():
-	control_instance = control_script.new()
-	get_tree().root.add_child(control_instance)
-	control_instance.host_button("")
+	var server_instance = server_script.new()
+	get_tree().root.add_child(server_instance)
+	server_instance.start_server()  # Assuming there's a method named 'start_server' in your Server.gd
+	pass # Replace with function body.
 
 
 func _on_line_edit_property_list_changed():
@@ -24,22 +23,10 @@ func _on_line_edit_property_list_changed():
 
 
 func _on_join_lobby_pressed():
-	control_instance = control_script.new()
-	get_tree().root.add_child(control_instance)
-	control_instance.join_button()
 	# initialize as client
 	pass # Replace with function body.
 
 
 func _on_host_internet_pressed():
-	var server_instance = server_script.new()
-	get_tree().root.add_child(server_instance)
-	server_instance.start_server()  # Assuming there's a method named 'start_server' in your Server.gd
-	pass # Replace with function body.
 	# Start game StartGame.rpc()
-	pass # Replace with function body.
-
-
-func _on_start_game_button_down():
-	control_instance.StartGame()
 	pass # Replace with function body.
