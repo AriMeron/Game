9c3d74b848d43cdb3078de1fa7bcafe7235fbecb
WilliamAbraham <williamjohnabraham1@gmail.com>
2024-01-26 14:37:05 -0800

    bullet collision

--- Scenes/Characters/Joe Biden/Gun/bullet.gd
+++ Scenes/Characters/Joe Biden/Gun/bullet.gd
@@ -3,10 +3,13 @@ extends CharacterBody3D
 var lifetime = 5.0  # Lifetime of the bullet in seconds
 var initial_direction: Vector3  # Define the initial_direction property
 var active = false
+var bullet
 
 func _ready():
 	set_process(true)
 	active = true
+	bullet = $Area3D
+	bullet.connect("body_entered", Callable(self, "_on_Area_body_entered"))
 
 func _process(delta):
 	if active:
@@ -15,3 +18,7 @@ func _process(delta):
 	lifetime -= delta
 	if lifetime <= 0:
 		queue_free()  # Remove the bullet from the scene
+
+func _on_Area_body_entered(body:Node) -> void:
+	print("test")
+	queue_free()
